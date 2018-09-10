16662dbd235e9192d39b1c28e80c29c016a61921
Hannah Smith <hannah@highrise.digital>
2018-09-10 18:21:40 +0100

    Get loop to pull chamber data from Rental Chambers page

--- partials/loops/loop-chambers.php
+++ partials/loops/loop-chambers.php
@@ -1,25 +1,30 @@
 <?php
 
-$query_args = array(
-	'post_type' => 'chamber',
-	'posts_per_page' => -1
-);
-
-$count = 0;
-
-$result = new WP_Query( $query_args );
-
-if ( $result->have_posts() ) : ?>
+// check if the repeater field has rows of data
+if( have_rows('chambers') ): ?>
 
 	<h2>Chambers for hire</h2>
+	<?php get_template_part ( 'partials/loop-content/content-chambers-list-headings'); ?>
 
-	<?php while ( $result->have_posts() ) : $result->the_post(); 
+	<?php // loop through the rows of data
+    while ( have_rows('chambers') ) : the_row();
+
+		$post_object = get_sub_field ( 'chamber' );
+
+		if( $post_object ): 
+
+			// override $post
+			$post = $post_object;
+			setup_postdata( $post ); 
 	
-		include( locate_template( 'partials/loop-content/content-chambers-list.php' ) ); 
-		$count++;
+			include( locate_template( 'partials/loop-content/content-chambers-list.php' ) ); 
+			$count++;
 
-	endwhile;
+			wp_reset_postdata();
 
-	wp_reset_query();
+		endif; 
+
+    endwhile;
+
 endif; 
 
